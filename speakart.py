3bf4b3ff8d8e1c6d450e2e25fb9f7f58f5c778bc
Tobi John <tobioluwafemi58@gmail.com>
2022-09-17 13:26:36 +0100

    defines upload function

--- speakart.py
+++ speakart.py
@@ -15,5 +15,20 @@ transcripting_endpoint = "https://api.assemblyai.com/v2/transcript"
 
 headers = {
     'Authorization': assemblyai_key,
-
+    'content-type': 'application/json',
 }
+
+def assemblyai_upload(file):
+
+    def get_upload_url(file):
+        
+        with open(file, 'rb') as f:
+            while True:
+                data = f.read(5242880)
+                if not data:
+                    break
+                yield data
+    
+    upload_response = requests.post(upload_endpoint, header=headers, data=get_upload_url(file))
+
+    return upload_response.json()['upload_url']
