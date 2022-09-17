2f81caa029f2f89231706949425bb1443451373e
Tobi John <tobioluwafemi58@gmail.com>
2022-09-17 13:18:01 +0100

    adds key and updates readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # AssemblyAI
+
+
+## What is AssemblyAI?
+
+
+
+## Storing your API key
+A standard and safe way to store your API key is by:
+echo "export OPENAI_API_KEY='yourkey'" >> ~/.bash_profile
+echo "export ASSEMBLY_API_KEY='yourkey'" >> ~/.bash_profile
+
+where 'yourkey' is your API key. And '~/.bash_profile' loads the key into your environment.
--- speakart.py
+++ speakart.py
@@ -1,7 +1,19 @@
 import os 
+import time
 import requests
 import streamlit as st
+import openai
 import streamlit.components.v1 as components
 
 
-openai_key = os.environ.get('OPENAI_API_KEY')
+openai_key = os.environ['OPENAI_API_KEY']
+assemblyai_key = os.environ['ASSEMBLYAI_API_KEY']
+
+
+upload_endpoint = "https://api.assemblyai.com/v2/upload"
+transcripting_endpoint = "https://api.assemblyai.com/v2/transcript"
+
+headers = {
+    'Authorization': assemblyai_key,
+
+}
