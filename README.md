74f89ddb12b87cbfb3a1fd9924a20933ce3e338b
Tobi John <tobioluwafemi58@gmail.com>
2022-09-18 15:57:20 +0100

    fixes bugs

--- README.md
+++ README.md
@@ -17,3 +17,8 @@ echo "export OPENAI_API_KEY='yourkey'" >> ~/.bash_profile
 echo "export ASSEMBLY_API_KEY='yourkey'" >> ~/.bash_profile
 
 where 'yourkey' is your API key. And '~/.bash_profile' loads the key into your environment.
+
+to view this Streamlit app on a browser, run it with the following
+  command:
+
+    streamlit run speakart.py [ARGUMENTS]
--- speakart.py
+++ speakart.py
@@ -1,14 +1,19 @@
 import os 
 import time
+from tkinter.ttk import Style
 import requests
 from io import BytesIO
 import numpy as np
+from PIL import Image
+import tkinter
+import matplotlib
+import pandas as pd
+import PIL.Image
 import streamlit as st
-import openai
 from docarray import Document
-from dalle2 import Dalle2
 import streamlit.components.v1 as components
-
+from IPython.display import Image, display
+#matplotlib.use('MacOSX')
 
 
 # DESIGN implement changes to the standard streamlit UI/UX
@@ -35,18 +40,21 @@ def record_audio():
     st_audiorec = components.declare_component("st_audiorec", path=build_dir)
 
     # TITLE and Creator information
-    st.title('streamlit audio recorder')
-    st.markdown('Implemented by '
-        '[Stefan Rummer](https://www.linkedin.com/in/stefanrmmr/) - '
+    st.title("Speaking Art into Existence \U0001F58C")
+    st.markdown( 
+        f'Powered by  <img src="https://www.assemblyai.com/_next/static/media/light.f35331fb.svg" width="100" height="100">',
+        unsafe_allow_html=True)
+    st.markdown('An AssemblyAI Speech-to-Text API Demo Implemented by '
+        '[Tobi John (MLOps_engineer)](https://twitter.com/MLOps_engineer) - '
         'view project source code on '
-        '[GitHub](https://github.com/stefanrmmr/streamlit_audio_recorder)')
+        '[GitHub](https://github.com/tobsiee/AssemblyAI)')
     st.write('\n\n')
 
 
     # STREAMLIT AUDIO RECORDER Instance
     val = st_audiorec()
     # web component returns arraybuffer from WAV-blob
-    st.write('Audio data received in the Python backend will appear below this message ...')
+    #st.write('Audio data received in the Python backend will appear below this message ...')
 
     if isinstance(val, dict):  # retrieve audio data
         with st.spinner('retrieving audio-recording...'):
@@ -59,12 +67,18 @@ def record_audio():
 
         # wav_bytes contains audio data in format to be further processed
         # display audio data as received on the Python side
-        st.audio(wav_bytes, format='audio/wav')
+        #st.audio(wav_bytes, format='audio/wav')
+
+        # save audio data to file
+        with open('input.wav', 'wb') as f:
+            f.write(wav_bytes)
+
+    
 
 
 
-openai_key = os.environ['OPENAI_API_KEY']
-assemblyai_key = os.environ['ASSEMBLYAI_API_KEY']
+assemblyai_key = os.environ.get('ASSEMBLYAI_API_KEY')
+assemblyai_key = 'cc5bbff100b9482d9766dedca9597dc1'
 
 
 upload_endpoint = "https://api.assemblyai.com/v2/upload"
@@ -86,8 +100,8 @@ def assemblyai_upload(file):
                     break
                 yield data
     
-    upload_response = requests.post(upload_endpoint, header=headers, data=get_upload_url(file))
-
+    upload_response = requests.post(upload_endpoint, headers=headers, data=get_upload_url(file))
+    #st.write()
     return upload_response.json()['upload_url']
 
 def transcribe(upload_url):
@@ -96,7 +110,7 @@ def transcribe(upload_url):
 
     transcribe_response = requests.post(transcripting_endpoint, json=json, headers=headers)
     transcription_id = transcribe_response.json()['id']
-
+    st.write('AssemblyAI has received your prompt and is now transcribing \U0001F642')
     return transcription_id 
 
 def get_transcription_result(transcription_id):
@@ -119,52 +133,58 @@ def call_dalle(prompt):
      
     server_url = 'grpcs://dalle-flow.dev.jina.ai'
 
-    doc = Document(text=prompt).post(server_url, parameters={'num_images': 2})
+    doc = Document(text=prompt).post(server_url, parameters={'num_images': 6})
     da = doc.matches
 
     da.plot_image_sprites(fig_size=(10,10), show_index=True)
 
-    fav_id = 3
+    fav_id = 4
 
     fav = da[fav_id]
     fav.embedding = doc.embedding
 
-    time.sleep(1)
+   
 
     diffused = fav.post(f'{server_url}', parameters={'skip_rate': 0.6, 'num_images': 4}, target_executor='diffusion').matches
 
-    diffused.plot_image_sprites(fig_size=(10,10), show_index=True)
+    diffused.plot_image_sprites(fig_size=(2,2), show_index=True)
 
-    dfav_id = 2
+    dfav_id = 1
     fav = diffused[dfav_id]
 
     fav = fav.post(f'{server_url}/upscale')
-    fav.display()
 
+    d =(  
+         Document(uri=fav.uri)
+        .load_uri_to_image_tensor()
+        .set_image_tensor_shape(shape=(224, 224)) 
+        .set_image_tensor_normalization() 
+        .set_image_tensor_channel_axis(-1, 0) 
+        )
+    d.save_image_tensor_to_file('image.png', channel_axis=0)
+    image = PIL.Image.open('image.png')
+    st.image(image, caption="Generated Image", use_column_width=True)
 
 
 def main():
-
-    st.title("Speaking Art into Existence")
-    file = "input.wav"
-
-    record_audio(file)
+    record_audio()
+    
+    # making sure the file has been updated
+    time.sleep(5)
 
     upload_url = assemblyai_upload(file)
-    st.write('AssemblyAI has received your prompt')
-
+ 
     transcription_id = transcribe(upload_url)
-    st.write('AssemblyAI is transcribing your prompt')
-
+   
     prompt = get_transcription_result(transcription_id)
-    st.write('AssemblyAI has transcribed your prompt')
     st.info(prompt)
 
-    image_output = call_dalle(prompt)
-    st.write('Your image has been generated generated your image')
+    call_dalle(prompt)
+    st.write('Your image has been generated as shown above \U0001F600')
+   
 
 
 
 if __name__ == "__main__":
+    file = "input.wav"
     main()
-    
